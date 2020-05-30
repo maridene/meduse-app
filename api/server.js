641a994c3f2b1d2d521e020bfd516761ace905bb
medAziz <medaziz.ridene@gmail.com>
2020-05-30 16:44:42 +0100

    change allowed access domain port

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
   next();
 });
 
