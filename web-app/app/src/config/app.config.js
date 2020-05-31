5bd33d30bcceea840c078afeb4cf26ec6951d1fc
medAziz <medaziz.ridene@gmail.com>
2020-05-31 03:14:15 +0100

    disable use of html5 mode

--- web-app/app/src/config/app.config.js
+++ web-app/app/src/config/app.config.js
@@ -1,7 +1,7 @@
 function AppConfig($httpProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
   'ngInject';
 
-  $locationProvider.html5Mode(true);
+  //$locationProvider.html5Mode(true);
 
   $stateProvider
       .state('app', {
