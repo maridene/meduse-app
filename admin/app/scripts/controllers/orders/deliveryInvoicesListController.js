af52d39a89f3bdfddc44c54a88276f1ff6456003
maridene <medaziz.ridene@gmail.com>
2021-11-20 00:06:34 +0100

    make possible deleting credit, devis and delivery invoices

--- admin/app/scripts/controllers/orders/deliveryInvoicesListController.js
+++ admin/app/scripts/controllers/orders/deliveryInvoicesListController.js
@@ -7,53 +7,93 @@
  * Controller of the delivery invoices list page
  */
 
-angular.module('sbAdminApp').controller('DeliveryInvoicesListCtrl', ['$scope', 'RestService', 'UsersService', function ($scope, RestService, UsersService) {
-    $scope.getList = function() {
+angular.module('sbAdminApp').controller('DeliveryInvoicesListCtrl', ['$scope', 'RestService', 'UsersService', 'ModalService', function ($scope, RestService, UsersService, ModalService) {
+    function getList() {
         RestService.get('invoice/shipping')
             .then(function(result) {
                 $scope.list = result.data.map(function(each) {
-                    var url = SERVER_URL + '/static/invoices/' + each.filename;
-                    var id = ('').concat('<a href=\"').concat(url).concat('\">').concat(each.number).concat('</a>');
+                    var invoiceUrl = SERVER_URL + '/static/invoices/' + each.filename;
                     var orderLink = '#/dashboard/order-details/' + each.orderId;
                     var refOrder = ('').concat('<a href=\"').concat(orderLink).concat('\">').concat(each.orderRef).concat('</a>');
                     var clientId = each.filename.split('-')[3];
                     return {
-                        id: id,
+                        id: each.number,
+                        url: ('').concat('<a href=\"').concat(invoiceUrl).concat('\">').concat(each.number).concat('</a>'),
                         client: clientId,
                         refOrder: refOrder,
+                        filename: each.filename
                     };
                 });
-
                 UsersService.getAllClients().then(function (clients) {
                     $scope.list.forEach(function (row) {
                         row.client = findClientNameById(row.client, clients);
                     });
-                    $scope.buildTable();
+                    $scope.filteredInvoices = $scope.list;
+                    $scope.$watch('filterQuery', filterInvoices);
                 }, function () {
-                    $scope.buildTable();
                 });
+
             });
+      };
+    function filterInvoices() {
+      $scope.filteredInvoices = $scope.list.filter(function(item) {
+        return item.client.toUpperCase().includes($scope.filterQuery.toUpperCase())
+        || item.refOrder.toUpperCase().includes($scope.filterQuery.toUpperCase())
+        || item.id.includes($scope.filterQuery);
+      });
+      $scope.updateState();
+    };
 
-            const findClientNameById = function (id, list) {
-                var found = list.filter(function (each) {
-                    return '' + each.id === '' + id;
-                });
-                if (found.length) {
-                    return found[0].name;
-                } else {
-                    return '-';
-                }
-            };
+    function init() {
+        $scope.filteredInvoices = [];
+        $scope.filterQuery = '';
+        getList();
+        $scope.deleteDisabled = true;
+    }
 
-            $scope.buildTable = function()  {
-                $scope.table = angular.element(document.querySelector('#table-subscribers'));
-                $scope.table.bootstrapTable('destroy')
-                            .bootstrapTable({
-                                data: $scope.list
-                            });
-            };
-  };
+    function findClientNameById(id, list) {
+        var found = list.filter(function (each) {
+            return '' + each.id === '' + id;
+        });
+        if (found.length) {
+            return found[0].name;
+        } else {
+            return '-';
+        }
+    };
 
-  $scope.getList();
+    $scope.delete = function(item) {
+        doDelete([item.filename])
+            .then(function() {
+              ModalService.showSuccessModal(INVOICES_DELETED_SUCCESS);
+              init();
+            });
+    };
+
+    $scope.deleteSelected = function() {
+        var selected = $scope.filteredInvoices.filter(function(each) {
+            return each.isSelected;
+        }).map(function(each) {
+            return each.filename;
+        });
+        doDelete(selected)
+            .then(function() {
+              ModalService.showSuccessModal(INVOICES_DELETED_SUCCESS);
+              init();
+            });
+    };
+
+     function doDelete(files) {
+        var data = {files: files.join(',')};
+        return RestService.post("invoice/delete", data);
+    };
+
+    $scope.updateState = function() {
+        $scope.deleteDisabled = $scope.filteredInvoices.filter(function (each) {
+            return each.isSelected;
+        }).length === 0;
+    };
+
+    init();
 
 }]);
