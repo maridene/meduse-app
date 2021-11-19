e60752be02590fbe705f069ab21d7e843982d256
maridene <medaziz.ridene@gmail.com>
2021-11-20 00:05:57 +0100

    fix file indentation

--- admin/app/scripts/controllers/orders/invoicesListController.js
+++ admin/app/scripts/controllers/orders/invoicesListController.js
@@ -9,31 +9,31 @@
 
 angular.module('sbAdminApp').controller('InvoicesListCtrl', ['$scope', 'RestService', 'UsersService', 'ModalService', function ($scope, RestService, UsersService, ModalService) {
     function getList() {
-            RestService.get('invoice/invoices')
-                .then(function(result) {
-                    $scope.list = result.data.map(function(each) {
-                        var invoiceUrl = SERVER_URL + '/static/invoices/' + each.filename;
-                        var orderLink = '#/dashboard/order-details/' + each.orderId;
-                        var refOrder = ('').concat('<a href=\"').concat(orderLink).concat('\">').concat(each.orderRef).concat('</a>');
-                        var clientId = each.filename.split('-')[3];
-                        return {
-                            id: each.number,
-                            url: ('').concat('<a href=\"').concat(invoiceUrl).concat('\">').concat(each.number).concat('</a>'),
-                            client: clientId,
-                            refOrder: refOrder,
-                            filename: each.filename
-                        };
-                    });
-                    UsersService.getAllClients().then(function (clients) {
-                        $scope.list.forEach(function (row) {
-                            row.client = findClientNameById(row.client, clients);
-                        });
-                        $scope.filteredInvoices = $scope.list;
-                        $scope.$watch('filterQuery', filterInvoices);
-                    }, function () {
-                    });
-
+        RestService.get('invoice/invoices')
+            .then(function(result) {
+                $scope.list = result.data.map(function(each) {
+                    var invoiceUrl = SERVER_URL + '/static/invoices/' + each.filename;
+                    var orderLink = '#/dashboard/order-details/' + each.orderId;
+                    var refOrder = ('').concat('<a href=\"').concat(orderLink).concat('\">').concat(each.orderRef).concat('</a>');
+                    var clientId = each.filename.split('-')[3];
+                    return {
+                        id: each.number,
+                        url: ('').concat('<a href=\"').concat(invoiceUrl).concat('\">').concat(each.number).concat('</a>'),
+                        client: clientId,
+                        refOrder: refOrder,
+                        filename: each.filename
+                    };
                 });
+                UsersService.getAllClients().then(function (clients) {
+                    $scope.list.forEach(function (row) {
+                        row.client = findClientNameById(row.client, clients);
+                    });
+                    $scope.filteredInvoices = $scope.list;
+                    $scope.$watch('filterQuery', filterInvoices);
+                }, function () {
+                });
+
+            });
       };
     function filterInvoices() {
       $scope.filteredInvoices = $scope.list.filter(function(item) {
